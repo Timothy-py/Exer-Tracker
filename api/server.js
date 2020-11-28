959e1f67261aeca29f341cea27fe069ec60b8d27
Timothy-py <adeyeyetimothy33@gmail.com>
2020-11-28 11:42:01 +0100

    configured mongodb database connnection

--- api/server.js
+++ api/server.js
@@ -9,6 +9,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// setup mongodb connection: connecting to mongo atlas
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex: true, useUnifiedTopology:true});
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log('MongoDB Database connection established successfully');
+})
+
 
 app.listen(port, ()=>{
     console.log(`Server is running on port: ${port}`);
