7f6b6f2ddf67b9d8f6f8f164e0ce125fee9ed039
Timothy-py <adeyeyetimothy33@gmail.com>
2020-11-28 15:58:59 +0100

    added exercise logics

---
+++ api/controllers/exercise.js
@@ -0,0 +1,25 @@
+const {Exercise} = require('../models/exercise');
+
+exports.getAllExercises = (req, res) => {
+    Exercise.find()
+    .then(exercises => res.json(exercises))
+    .catch(err => res.status(400).json('Error: '+ err));
+};
+
+exports.addNewExercise = (req, res) => {
+    const username = req.body.username;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const date = Date.parse(req.body.date);
+
+    const newExercise = new Exercise({
+        username,
+        description,
+        duration,
+        date
+    })
+
+    newExercise.save()
+    .then(() => res.json('Exercise added!'))
+    .catch(err => res.status(400).json('Error: ' + err))
+};
