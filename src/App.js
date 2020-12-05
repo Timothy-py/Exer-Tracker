91732f48954688a6d92a0ed42b962947d43813d7
Timothy-py <adeyeyetimothy33@gmail.com>
2020-12-05 19:34:10 +0100

    coded the component for creating user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import {BrowserRouter as Router, Rout} from "react-router-dom"
+import {BrowserRouter as Router, Route} from "react-router-dom"
 import './App.css';
 
 import Navbar from "./components/navbar"
@@ -12,12 +12,14 @@ function App() {
   return (
     
     <Router>
-      <Navbar />
-      <br/>
-      <Route path="/" exact component={ExercisesList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
     </Router>
 
   );
---
+++ src/components/create-user.js
@@ -0,0 +1,57 @@
+import React, {Component} from "react"
+
+
+class CreateUser extends Component{
+
+    constructor(){
+        super()
+        this.state = {
+            username: ""
+        }
+
+        this.changeHandler = this.changeHandler.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    changeHandler(event){
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    onSubmit(event){
+        event.preventDefault();
+
+        const user = {
+            username: this.state.username
+        }
+        console.log(user)
+        this.setState({
+            username: ""
+        })
+    }
+
+    render() {
+        return (
+             <div>
+                 <h3>Create New User</h3>
+                 <form onSubmit={this.onSubmit}>
+                    
+                    <div className="form-group">
+                        <label>Username:</label>
+                        <input type="text" required className="form-control" 
+                        name="username" value={this.state.username} onChange={this.changeHandler}/>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="submit" value="Create User" className="btn btn-primary" />
+                    </div>
+                 </form>
+             </div>
+        );
+    }
+
+}
+
+export default CreateUser;
