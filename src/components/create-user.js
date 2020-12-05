6c0f5bc0c18866ad18d30565cb17f511a626ac2c
Timothy-py <adeyeyetimothy33@gmail.com>
2020-12-05 21:53:30 +0100

    connected the create user component to the backend

--- src/components/create-user.js
+++ src/components/create-user.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react"
+import axios from "axios"
 
 
 class CreateUser extends Component{
@@ -27,6 +28,11 @@ class CreateUser extends Component{
             username: this.state.username
         }
         console.log(user)
+
+        axios.post('http://localhost:5000/exer-tracker/user/add', user)
+        .then(res => console.log(res.data))
+        .catch(err => console.log(`Unable to add User: ${err}`))
+
         this.setState({
             username: ""
         })
