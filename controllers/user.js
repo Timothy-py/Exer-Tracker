1ec5d191207337d3535b47e50a8ca3f6c1b549fa
Timothy-py <adeyeyetimothy33@gmail.com>
2020-12-08 21:52:20 +0100

    api developed completely

---
+++ controllers/user.js
@@ -0,0 +1,26 @@
+const User = require('../models/user');
+
+exports.getAllUsers = (req, res)=>{
+    User.find()
+    .then(users=>{
+        res.json({
+            status: true,
+            message: "All Users Retrieved Successfully",
+            data: users,
+        })
+    })
+    .catch(err=>{
+        res.status(400).json('Error: ' + err)
+    })
+};
+
+
+exports.addNewUser = (req, res, next)=>{
+    const username = req.body.username;
+    console.log(username)
+    const newUser = new User({username});
+
+    newUser.save()
+    .then(() => res.json('User added'))
+    .catch(err => res.status(400).json('Error: ' + err));
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// require routes
+const exer_tracker = require('./routes/exer-tracker');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/exer-tracker', exer_tracker);
+
+// setup mongodb connection: connecting to mongo atlas
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex: true, useUnifiedTopology:true});
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log('MongoDB Database connection established successfully');
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port: ${port}`);
+});
+
+module.exports = app;
